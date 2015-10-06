d9204fbb3c4f8b8a48f8c39d6c785116ede7f7c3
Louis Alridge <louis@hiplogiq.com>
2015-10-06 12:23:36 -0500

    1.0.3 bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'SocialCentiv'
 maintainer_email 'louis@socialcentiv.com'
 license          'All Rights Reserved'
 description      'Installs and configures various environments'
-version          '1.0.2'
+version          '1.0.3'
 
 depends 'ohai'
 depends 'build-essential'
